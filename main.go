22b40f9a372d2fc9c99f4a17d471d8491c958113
Panupong Rong-O <tayalone@gmail.com>
2022-09-12 11:57:05 +0700

    ✨ สร้าง Function

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
-package main
+package mylogger
 
-import "fmt"
+import (
+	"log"
+)
 
-func main() {
-	fmt.Println("Welcome to my first package")
+/*
+LogInfo print logingo
+*/
+func LogInfo(m string) {
+	log.Printf("INFO - %v", m)
 }
